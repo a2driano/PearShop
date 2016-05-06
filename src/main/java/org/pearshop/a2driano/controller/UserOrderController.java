d95181bf7f1daacf8992491c3ea9a959ea14fb60
Администратор <a2driano@gmail.com>
2016-05-06 18:44:13 +0300

    add order controller

--- src/main/java/org/pearshop/a2driano/controller/UserOrderController.java
+++ src/main/java/org/pearshop/a2driano/controller/UserOrderController.java
@@ -1,11 +1,38 @@
 package org.pearshop.a2driano.controller;
 
+import org.pearshop.a2driano.model.web.UserOrderDTO;
+import org.pearshop.a2driano.service.UserOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @version 1.0
  * @autor a2driano
  * @project: pearshop
  * @since 06.05.2016
  */
+@Controller
 public class UserOrderController {
+    @Autowired
+    private UserOrderService userOrderService;
 
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ModelAndView getAllUserOrders() {
+        ModelAndView modelAndView = new ModelAndView("admin");
+        modelAndView.addObject("userOrderList", userOrderService.getUserOrderList());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/addOrder", method = RequestMethod.POST)
+    public void sendOrder(UserOrderDTO userOrderDTO) {
+        userOrderService.addUserOrder(userOrderDTO);
+    }
+
+    @RequestMapping(value = "/updateOrder", method = RequestMethod.POST)
+    public void updateOrder(UserOrderDTO userOrderDTO) {
+        userOrderService.updateUserOrder(userOrderDTO);
+    }
 }
